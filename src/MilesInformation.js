0893e11ac65fb20e8d7c0417c62dbdfced39de79
Christian Scott <christianfscott@gmail.com>
2018-08-14 00:46:34 +1200

    fix warnings

--- src/MilesInformation.js
+++ src/MilesInformation.js
@@ -5,7 +5,7 @@ import {withConfigValues} from './Configuration'
 export const MilesInformation = withConfigValues(configValues => (
   <React.Fragment>
     {['60', '60D', '75', '75D', '90D', 'P100D'].map(model => (
-      <span>{model}: {getRange({model, ...configValues})} miles, </span>
+      <span key={model}>{model}: {getRange({model, ...configValues})} miles, </span>
     ))}
   </React.Fragment>
 ))
