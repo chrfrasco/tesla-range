4732331d4cc523402f3ad59823059855001b734e
Christian Scott <christianfscott@gmail.com>
2018-08-15 01:07:53 +1200

    react-snap bug workaround

--- src/Tesla/Wheels.js
+++ src/Tesla/Wheels.js
@@ -8,10 +8,10 @@ const spin = keyframes`
 
 const getRotationDuration = speed => 0.8 - ((speed - 45) / 62.5)
 
+/* !important used due to react-snap bug */
 const WheelWrapper = styled(FixedAspectRatio)`
-  width: 9%;
-
-  position: absolute;
+  width: 9% !important;
+  position: absolute !important;
   top: 48%;
 
   animation: ${spin} ${({speed}) => getRotationDuration(speed)}s infinite linear;
