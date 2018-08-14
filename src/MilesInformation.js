ae9e9e2409ea4b67250c8786a5e1f304aa2f0c0a
Christian Scott <christianfscott@gmail.com>
2018-08-14 23:44:53 +1200

    Add data table

--- src/MilesInformation.js
+++ src/MilesInformation.js
@@ -1,11 +1,53 @@
 import React from 'react'
 import {getRange} from './utils'
+import styled from 'styled-components'
 import {withConfigValues} from './Configuration'
 
+const TableWrapper = styled.div`
+  margin-bottom: 2rem;
+  border: thin solid #333;
+  border-radius: 3px;
+  overflow: hidden;
+`
+
+const Table = styled.table`
+  margin: -1px;
+  width: calc(100% + 2px);
+  border-collapse: collapse;
+`
+
+const HeaderCell = styled.th`
+  border: thin solid #888;
+  padding: 6px 12px;
+  text-align: left;
+`
+
+const DataCell = styled.td`
+  border: thin solid #888;
+  padding: 6px 12px;
+  width: 16.67%;
+`
+
+const teslaModels = ['60', '60D', '75', '75D', '90D', 'P100D']
 export const MilesInformation = withConfigValues(configValues => (
-  <React.Fragment>
-    {['60', '60D', '75', '75D', '90D', 'P100D'].map(model => (
-      <span key={model}>{model}: {getRange({model, ...configValues})} miles, </span>
-    ))}
-  </React.Fragment>
+  <TableWrapper>
+  <Table>
+    <thead>
+      <tr>
+        {teslaModels.map(model => (
+          <HeaderCell key={model}>{model}</HeaderCell>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        {teslaModels.map(model => (
+          <DataCell key={model}>
+            {getRange({model, ...configValues})} miles
+          </DataCell>
+        ))}
+      </tr>
+    </tbody>
+  </Table>
+  </TableWrapper>
 ))
